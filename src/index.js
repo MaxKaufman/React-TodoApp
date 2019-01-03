fb0f2d01123880201f388ae8c001eb28d5b654d3
Alexandr_Sakh <AlexandrSakharoff@gmail.com>
2019-01-04 02:53:18 +0300

    Start work with JSX

--- src/index.js
+++ src/index.js
@@ -1,34 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const todoItems = ['Learn React', 'Build awesome app'];
+
 const AppHeader = () => {
-    return <h1>My Todo List</h1>
+    return <h1>My Todho
+        List</h1>
 };
 
 const SearchPanel = () => {
-    return <input placeholder='search'/>
+    const searchText = 'Type to search';
+    const searchStyle = {
+        fontSize: '18px'
+    };
+
+    return <input
+        placeholder={searchText}
+        style={searchStyle}
+        disabled
+    />
 
 };
 
 const TodoList = () => {
     return (
         <ul>
-            <li>Learn React</li>
-            <li>Build Awesome App</li>
+            <li>{ todoItems[0] }</li>
+            <li>{ todoItems[1] }</li>
         </ul>
     );
 };
 
 const App = () => {
+    const isLoggedIn = true;
+    const loginBox = <span>Login please</span>;
+    const welcomeBox = <span>Welcome back</span>;
+
     return (
         <>
+            { isLoggedIn ? welcomeBox : loginBox }
             <AppHeader/>
             <SearchPanel/>
-            <TodoList/>
+            <TodoList items={['item1', 'item2']} />
         </>
     );
 };
 
-const el = <App />;
+//const el = <App />;
 
 ReactDOM.render(<App />, document.getElementById('root'));
